b469b010a5f3ccaafe37c81fd8887f47427f9a43
Mosheh EliYahu <mosheh.eliyahu@gmail.com>
2020-07-16 13:07:41 +0930

    Feat: added useState hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 import WeatherCard from "./components/WeatherCard/component";
 
 function App() {
+  const APPID = "";
+  const [city, setCity] = useState("London, gb");
+  const [condition, setCondition] = useState("");
+  const [country, setCountry] = useState("");
+  const [temp, setTemp] = useState("");
+
+  const data = async () => {
+    const apiRes = await fetch(
+      `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=${APPID}`
+    );
+    const resJSON = await apiRes.json();
+    return resJSON;
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    data().then((res) => {
+      setCity(res.city.name);
+      setCountry(res.city.country);
+      setTemp(res.list[0].main.temp);
+      setCondition(res.list[0].weather[0].main);
+      // console.log(res);
+    });
+  };
   return (
     <div className="App">
-      <WeatherCard temp={14} condition="Rain" city="Sydney" country="AU" />
-      <WeatherCard temp={20} condition="Tornado" city="Auklands" country="NZ" />
-      <WeatherCard temp={40} condition="Haze" city="London" country="GB" />
-      <WeatherCard temp={10} condition="Dust" city="Cape Town" country="SA" />
+      <WeatherCard
+        temp={temp}
+        condition={condition}
+        city={city}
+        country={country}
+      />
+      <form>
+        <input value={city} onChange={(e) => setCity(e.target.value)} />
+        <button onClick={(e) => handleSearch(e)}>Search</button>
+      </form>
     </div>
   );
 }
